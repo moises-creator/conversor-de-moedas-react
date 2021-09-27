e3e072ecd8f9ee5bf53bcf3c128180506ac78df8
Moisés Ximenes <moises.ximenes5@gmail.com>
2021-09-27 09:42:35 -0300

    Subindo aplicação react.js

---
+++ src/components/App3.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+class App3 extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {nome: undefined, txtNome: ''};   
+        
+    }
+
+    changetxtName =(event)=>{
+        this.setState({txtNome: event.target.value})
+    }
+
+    componentDidMount = () => {
+        const nome = sessionStorage.getItem('nome');
+        if(nome) this.setState({nome})
+    }
+
+    persistName = ()=> {
+        this.setState({nome: this.state.txtNome})
+        sessionStorage.setItem('nome', this.state.txtNome);
+    }
+
+
+  render() {
+      const renderForm = ()=> {
+       return (
+        <>
+        Nome: <input type="text" value={this.state.nome} onChange={this.changetxtName}></input>
+        <button onClick={this.persistName}>Salvar</button>
+        </>
+       ) 
+      }
+
+      const renderTxt = ()=> {
+        return (
+            <div>
+                
+                <p>Olá {this.state.nome}</p>
+               
+            </div>
+             );
+      }
+
+      return !this.state.nome ? renderForm() : renderTxt()
+    
+  }
+}
+
+
+export default App3;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+
+import App3 from './components/App3';
+import Formulario from './components/Formulario';
+import Hello from './components/Hello';
+import {Lista, Item} from './components/Lista';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+
+ 
+   
+<App/>
+    
+      
+   
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
